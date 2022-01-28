c189b2ed2633d87c3f009d2c5e34012d0b0ee49e
schaze <thomas_schaz@gmx.de>
2022-01-28 16:58:31 +0100

    upgraded to hc-node-homie-smarthome v0.5.0

--- src/deconzhomie/nodes/GestureNode.ts
+++ src/deconzhomie/nodes/GestureNode.ts
@@ -1,18 +1,20 @@
 import { HomieNode, HomieProperty, HomieDevice } from "node-homie";
 import { HomieNodeAtrributes, HOMIE_TYPE_BOOL, HOMIE_TYPE_INT } from "node-homie/model";
-import { H_SMARTHOME_TYPE_EXTENSTION } from "hc-node-homie-smarthome/model";
+import { BaseNodePropertyConfig, H_SMARTHOME_TYPE_EXTENSTION } from "hc-node-homie-smarthome/model";
+import { BaseSmarthomeNode } from "hc-node-homie-smarthome";
 
 export const H_SMARTHOME_TYPE_EXT_GUESTURE_SENSOR = `${H_SMARTHOME_TYPE_EXTENSTION}=guesturesensor`;
 
-export interface GestureSensorPropertyConfig {
+export const H_SMARTHOME_TYPE_EXT_GUESTURE_SENSOR_PROPS = ['gesture', 'button-event'] as const;
+export type SmarthomeTypeGuestureSensorProps = typeof H_SMARTHOME_TYPE_EXT_GUESTURE_SENSOR_PROPS[number];
+
+export interface GestureSensorPropertyConfig extends BaseNodePropertyConfig<SmarthomeTypeGuestureSensorProps> {
     gesture: boolean;
     buttonEvent: boolean;
 }
 
 
-export class GestureSensorNode extends HomieNode {
-    private propConfig: GestureSensorPropertyConfig;
-
+export class GestureSensorNode extends BaseSmarthomeNode<GestureSensorPropertyConfig> {
     public readonly propGesture: HomieProperty;
     public readonly propButtonEvent: HomieProperty;
 
@@ -45,28 +47,27 @@ export class GestureSensorNode extends HomieNode {
                 type: H_SMARTHOME_TYPE_EXT_GUESTURE_SENSOR
             },
             ...attrs
-        });
+        }, propConfig);
 
-        this.propConfig = propConfig;
 
         if (this.propConfig.gesture) {
-            this.propGesture = this.add(new HomieProperty(this, {
+            this.propGesture = this.makeProperty({
                 id: 'gesture',
                 name: 'Gesture event',
                 datatype: HOMIE_TYPE_INT,
                 retained: false,
                 settable: false,
-            }));
+            });
         }
 
         if (this.propConfig.buttonEvent) {
-            this.propButtonEvent = this.add(new HomieProperty(this, {
+            this.propButtonEvent = this.makeProperty({
                 id: 'button-event',
                 name: 'Button event',
                 datatype: HOMIE_TYPE_INT,
                 retained: false,
                 settable: false,
-            }));
+            });
         }
 
     }
--- src/deconzhomie/nodes/SceneNode.ts
+++ src/deconzhomie/nodes/SceneNode.ts
@@ -1,14 +1,18 @@
 import { HomieNode, HomieProperty, HomieDevice } from "node-homie";
 import { HomieNodeAtrributes, HOMIE_TYPE_ENUM} from "node-homie/model";
-import { H_SMARTHOME_TYPE_EXTENSTION } from "hc-node-homie-smarthome/model";
+import { BaseNodePropertyConfig, H_SMARTHOME_TYPE_EXTENSTION } from "hc-node-homie-smarthome/model";
+import { BaseSmarthomeNode } from "hc-node-homie-smarthome";
 
 export const H_SMARTHOME_TYPE_EXT_LIGHTSCENE = `${H_SMARTHOME_TYPE_EXTENSTION}=lightscene`;
 
-export interface LightSceneNodePropertyConfig {
+export const H_SMARTHOME_TYPE_EXT_LIGHTSCENE_PROPS = ['recall'] as const;
+export type SmarthomeTypeLightSceneProps = typeof H_SMARTHOME_TYPE_EXT_LIGHTSCENE_PROPS[number];
+
+export interface LightSceneNodePropertyConfig extends BaseNodePropertyConfig<SmarthomeTypeLightSceneProps> {
     scenes: string[];
 }
 
- export class LightSceneNode extends HomieNode {
+ export class LightSceneNode extends BaseSmarthomeNode<LightSceneNodePropertyConfig> {
     public readonly propConfig: LightSceneNodePropertyConfig;
 
     public readonly propRecall: HomieProperty;
@@ -27,17 +31,16 @@ export interface LightSceneNodePropertyConfig {
                 type: H_SMARTHOME_TYPE_EXT_LIGHTSCENE
             },
             ...attrs
-        });
-        this.propConfig = propConfig;
+        }, propConfig);
 
-        this.propRecall = this.add(new HomieProperty(this, {
+        this.propRecall = this.makeProperty({
             id: 'recall',
             name: 'Recall a scene',
             datatype: HOMIE_TYPE_ENUM,
             retained: false,
             settable: true,
             format: this.propConfig.scenes.join(',')
-        }));
+        });
 
 
 
